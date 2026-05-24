622434f965236dc22bcc62475d44347d489d7393
candrawij <candrawijayanto340@gmail.com>
2026-05-25 02:26:15 +0700

    fix filament document resource

--- app/Filament/Resources/DocumentResource.php
+++ app/Filament/Resources/DocumentResource.php
@@ -11,6 +11,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -52,7 +53,9 @@ class DocumentResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('badge')->label('Kategori'),
+                TextColumn::make('title')->label('Judul'),
+                TextColumn::make('description')->label('Deskripsi'),
             ])
             ->filters([
                 //
